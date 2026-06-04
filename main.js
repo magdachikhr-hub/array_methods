09f5c5adee0d265669f19fba7cc19612fe4fe96a
Magda Chikhradze <magda292005@gmail.com>
2026-06-04 16:23:22 +0400

    Object

--- main.js
+++ main.js
@@ -30,3 +30,58 @@ const newWordsArr = words.map((word) => {
 });
 
 console.log(newWordsArr);
+
+//some
+
+const result = arr1.some((num) => num > 100);
+console.log(result);
+
+const res1 = arr1.every((n) => n < 100);
+console.log(res1);
+
+const names = ["natia", "nino", "nluka", "nmagda"];
+
+const res2 = names.some((name) => name.startsWith("m"));
+
+console.log(res2);
+
+const res3 = names.every((name) => name.startsWith("n"));
+
+console.log(res3);
+
+//reverse
+
+console.log(names.reverse());
+
+//
+const user = {
+  name: "natia",
+  age: 23,
+};
+
+const game = {
+  genre: "sci-fi",
+  year: 1993,
+};
+console.log(Object.keys(user));
+
+//green - a class
+
+const food = {
+  spicy: "true",
+  sour: "false",
+};
+
+console.log(Object.keys(food));
+
+console.log(Object.values(game));
+
+console.log(Object.values(food));
+console.log(Object.entries(user));
+
+//
+console.log(Object.hasOwn(game, "year"));
+
+//ctrl and click to open library
+
+console.log(Object.hasOwn(user, "surname"));
